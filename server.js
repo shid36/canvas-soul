6c64dc0467bca22e0fd8c37a9427fd9188c5a47f
Shidratul Muntha Ribon <shidratulmuntharibon@gmail.com>
2026-07-06 23:37:09 +0600

    Connect Aiven MySQL

--- server.js
+++ server.js
@@ -7,7 +7,16 @@ const multer = require("multer");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const db = mysql.createConnection(process.env.DATABASE_URL);
+const db = mysql.createConnection({
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 db.connect((err) => {
   if (err) {
