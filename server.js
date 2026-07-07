6dfd9a054f9fd92e2c349b02bb43803dacc00c78
Shidratul Muntha Ribon <shidratulmuntharibon@gmail.com>
2026-07-07 19:43:27 +0600

    Fix server

--- server.js
+++ server.js
@@ -47,6 +47,7 @@ const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
     folder: "canvas-soul",
+      allowed_formats: ["jpg", "jpeg", "png", "heic","webp"],
   },
 });
 
